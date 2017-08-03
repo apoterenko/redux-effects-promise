8979ef1c499c34f25472be0a6a2a96807cde3b98
apoterenko <a.poterenko@gmail.com>
2017-08-03 13:15:54 +0300

    *refactoring

--- src/effects.middleware.ts
+++ src/effects.middleware.ts
@@ -3,10 +3,40 @@ import { AnyAction } from 'redux';
 import { EffectsService } from './effects.service';
 import { EffectsAction } from './effects.action';
 
+const toActions = (action: AnyAction, result: any, initialData: any): AnyAction[] => {
+	let actionsForDispatch: AnyAction[];
+	if (Array.isArray(result)) {
+		const thisIsNotArrayOfActionEffects =
+			!!(result as Array<any>).find(item => (!(item instanceof EffectsAction)));
+
+		if (!thisIsNotArrayOfActionEffects) {
+			actionsForDispatch = result.map(resultAction => ({
+				type: resultAction.type,
+				data: resultAction.data,
+				initialData: initialData
+			}));
+		}
+	} else if (result instanceof EffectsAction) {
+		const resultAction = result;
+		actionsForDispatch = [{
+			type: resultAction.type,
+			data: resultAction.data,
+			initialData: initialData
+		}];
+	}
+	actionsForDispatch = actionsForDispatch || [
+		{
+			type: `${action.type}.done`,
+			data: result,
+			initialData: initialData
+		}
+	];
+	return actionsForDispatch;
+};
+
 export const effectsMiddleware = ({dispatch}) => (
 	(next: Function) => (action: AnyAction) => {
 		const proxyFn = EffectsService.fromEffectsMap(action.type);
-
 		if (proxyFn) {
 			const proxyFnResult = proxyFn(action);
 			if (proxyFnResult instanceof Promise) {
@@ -15,42 +45,13 @@ export const effectsMiddleware = ({dispatch}) => (
 					type: action.type,
 					data: action.data,
 					promise: proxyFnResult.then(
-						result => {
-							if (Array.isArray(result)) {
-								const thisIsNotArrayOfActionEffects =
-									!!(result as Array<any>).find(item => (!(item instanceof EffectsAction)));
-
-								if (!thisIsNotArrayOfActionEffects) {
-									[].concat(result).forEach(resultAction => dispatch({
-										type: resultAction.type,
-										data: resultAction.data,
-										initialData: initialData
-									}));
-									return;
-								}
-							} else if (result instanceof EffectsAction) {
-								const resultAction = result;
-								dispatch({
-									type: resultAction.type,
-									data: resultAction.data,
-									initialData: initialData
-								});
-								return;
-							}
-
-							// Default behaviour
-							dispatch({
-								type: `${action.type}.done`,
-								data: result,
-								initialData: initialData
-							});
-						}
+						result => toActions(action, result, initialData).forEach((action) => dispatch(action))
 					)
 				});
 			} else if (proxyFnResult) {
-				const result = next(action);
+				const nextActionResult = next(action);
 				[].concat(proxyFnResult).forEach(action => dispatch({ type: action.type, data: action.data }));
-				return result;
+				return nextActionResult;
 			}
 		}
 		return next(action);

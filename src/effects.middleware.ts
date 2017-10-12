79886aebfc829c0da2415b0aaf5ef2740433d0cd
apoterenko <a.poterenko@gmail.com>
2017-10-12 18:46:52 +0300

    *formatting

--- src/effects.middleware.ts
+++ src/effects.middleware.ts
@@ -4,63 +4,63 @@ import { EffectsService } from './effects.service';
 import { EffectsAction } from './effects.action';
 
 const toActions = (action: AnyAction, result: any): AnyAction[] => {
-	const initialData = action.data;
-	let actionsForDispatch: AnyAction[];
+  const initialData = action.data;
+  let actionsForDispatch: AnyAction[];
 
-	if (Array.isArray(result)) {
-		const thisIsNotArrayOfActionEffects =
-			!!(result as Array<any>).find(item => (!(item instanceof EffectsAction)));
+  if (Array.isArray(result)) {
+    const thisIsNotArrayOfActionEffects =
+        !!(result as Array<any>).find(item => (!(item instanceof EffectsAction)));
 
-		if (!thisIsNotArrayOfActionEffects) {
-			actionsForDispatch = result.map(resultAction => ({
-				type: resultAction.type,
-				data: resultAction.data,
-				initialData: initialData
-			}));
-		}
-	} else if (result instanceof EffectsAction) {
-		const resultAction = result;
-		actionsForDispatch = [{
-			type: resultAction.type,
-			data: resultAction.data,
-			initialData: initialData
-		}];
-	}
-	actionsForDispatch = actionsForDispatch || [
-		{
-			type: `${action.type}.done`,
-			data: result,
-			initialData: initialData
-		}
-	];
-	return actionsForDispatch;
+    if (!thisIsNotArrayOfActionEffects) {
+      actionsForDispatch = result.map(resultAction => ({
+        type: resultAction.type,
+        data: resultAction.data,
+        initialData: initialData
+      }));
+    }
+  } else if (result instanceof EffectsAction) {
+    const resultAction = result;
+    actionsForDispatch = [{
+      type: resultAction.type,
+      data: resultAction.data,
+      initialData: initialData
+    }];
+  }
+  actionsForDispatch = actionsForDispatch || [
+    {
+      type: `${action.type}.done`,
+      data: result,
+      initialData: initialData
+    }
+  ];
+  return actionsForDispatch;
 };
 
 export const effectsMiddleware = ({dispatch}) => (
-	(next: Function) => (action: AnyAction) => {
-		const proxyFn = EffectsService.fromEffectsMap(action.type);
-		if (proxyFn) {
-			const proxyFnResult = proxyFn(action);
-			const initialData = action.data;
+    (next: Function) => (action: AnyAction) => {
+      const proxyFn = EffectsService.fromEffectsMap(action.type);
+      if (proxyFn) {
+        const proxyFnResult = proxyFn(action);
+        const initialData = action.data;
 
-			if (proxyFnResult instanceof Promise) {
-				return next({
-					type: action.type,
-					data: action.data,
-					promise: proxyFnResult.then(
-						result => toActions(action, result).forEach((action) => dispatch(
-							{ type: action.type, data: action.data, initialData: initialData }
-						)),
-						error => dispatch({ type: `${action.type}.error`, error: error, initialData: initialData })
-					)
-				});
-			} else if (proxyFnResult) {
-				const nextActionResult = next(action);
-				toActions(action, proxyFnResult).forEach((action) => dispatch(
-					{ type: action.type, data: action.data, initialData: initialData }
-				));
-				return nextActionResult;
-			}
-		}
-		return next(action);
-	});
+        if (proxyFnResult instanceof Promise) {
+          return next({
+            type: action.type,
+            data: action.data,
+            promise: proxyFnResult.then(
+                result => toActions(action, result).forEach((action) => dispatch(
+                    {type: action.type, data: action.data, initialData: initialData}
+                )),
+                error => dispatch({type: `${action.type}.error`, error: error, initialData: initialData})
+            )
+          });
+        } else if (proxyFnResult) {
+          const nextActionResult = next(action);
+          toActions(action, proxyFnResult).forEach((action) => dispatch(
+              {type: action.type, data: action.data, initialData: initialData}
+          ));
+          return nextActionResult;
+        }
+      }
+      return next(action);
+    });
